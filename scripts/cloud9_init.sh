616c8788fa93e21f82f9cd5bb76d31504e7ac86f
Thomas Berchtold, INI-EOS-EPS <thomas.berchtold@swisscom.com>
2024-05-02 21:55:57 +0200

    update command

--- scripts/cloud9_init.sh
+++ scripts/cloud9_init.sh
@@ -4,10 +4,25 @@
 #   Initialize Cloud9 instance
 #
 
-#
+# Perform general update
+sudo yum update -y
+
 #   Install and configure Terraform
-#
 wget https://artifactory.swisscom.com/artifactory/iaws-mastery-bin-generic-local/artifactory/general/tf/terraform_1.8.2_linux_amd64.zip
 unzip terraform_1.8.2_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
+
+cat <<EOF> ~/.terraformrc
+provider_installation {
+  direct {
+    exclude = ["registry.terraform.io/*/*"]
+  }
+  network_mirror {
+    url = "https://artifactory.swisscom.com/artifactory/api/terraform/terraform-remote/providers/"
+  }
+}
+EOF
+
+ 
+
 
--- tf_init/cloud9.tf
+++ tf_init/cloud9.tf
@@ -6,6 +6,9 @@ resource "aws_servicecatalog_provisioned_product" "iaws_cloud9" {
   path_name                  = "iaws-proof-of-concept"
   product_name               = "iaws-cloud9"
   provisioning_artifact_name = "1.0.4"
+  tags = {
+    mastery = "mongodb"
+  }
 
   provisioning_parameters {
     key   = "EnvironmentName"
@@ -17,9 +20,9 @@ resource "aws_servicecatalog_provisioned_product" "iaws_cloud9" {
     value = "S"
   }
 
-  depends_on = [
-    aws_servicecatalog_provisioned_product.join_shared_vpc
-  ]
+  # depends_on = [
+  #   aws_servicecatalog_provisioned_product.join_shared_vpc
+  # ]
 }
 
 resource "aws_ssm_document" "cloud9_document" {
@@ -35,23 +38,45 @@ resource "aws_ssm_document" "cloud9_document" {
       "name": "runShellScript",
       "inputs": {
         "runCommand": [
-          "sudo yum update -y",
           "git clone https://oauth2:glpat-dAeEAhdXJYs-5_JeJFrg@code.swisscom.com/swisscom/iaws-devops-mastery-friday/iaws-mongodb.git",
-          "cd iaws-mongodb",
-          "sudo ./install.sh"
+          "cd iaws-mongodb/scripts",
+          "chmod +x cloud9_init.sh",
+          "sudo ./cloud9_init.sh"
         ]
       }
     }
   ]
 }
 DOC
+  depends_on    = [aws_servicecatalog_provisioned_product.iaws_cloud9]
 }
 
+#
+# Lookup all existing ec2 instances in our account (which are now all provisioned Cloud9 instances)
+#
+data "aws_instances" "cloud9_instances" {
+  depends_on = [aws_servicecatalog_provisioned_product.iaws_cloud9]
+  instance_tags = {
+    Name = "*"
+  }
+}
+
+#
+# Associate the SSM document with the Cloud9 instances
+#
 resource "aws_ssm_association" "cloud9_ssm_association" {
   name = aws_ssm_document.cloud9_document.name
   targets {
     key    = "InstanceIds"
-    values = ["*"]
-
+    values = data.aws_instances.cloud9_instances.ids
   }
+  output_location {
+    s3_bucket_name = aws_s3_bucket.cloud9_document_bucket.bucket
+    s3_key_prefix  = "ssm-documents/my-ssm-document"
+  }
+}
+
+resource "aws_s3_bucket" "cloud9_document_bucket" {
+  bucket        = "mfriday-document-bucket"
+  force_destroy = true
 }
